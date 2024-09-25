8a824e2e10724af59be14137af131e6466a163d3
Lars André Roda Jansen <lajansen@stud.ntnu.no>
2024-09-25 11:58:08 +0200

    cuh

---
+++ tma4101-matematikk-1/ovinger/1-6 lineæralgebra 2/oppg2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.matrix([[2, 1, 1, 2],
+              [1, 0, 1, 1],
+              [1, -2, 1, -1],
+              [0, 1, 0, 1]])
+
+print(A)
+print(np.linalg.det(A))
+
+
