5e15513a0c9de54e6c9b8afd4fd3b764e841db4d
Lars André Roda Jansen <larsandre03@gmail.com>
2024-10-08 07:58:29 +0200

    yuh

---
+++ ttk4100-kybintro/gruppe18/matlab/rakett_init.m
@@ -0,0 +1,20 @@
+%INIT
+m = 100; %masse
+h = 5; %høyde
+d = 0.4; %diameter
+
+r = 0; %referanseverdi
+
+x_ddot = [0, 0]; %Startakselerasjon på 0 i x og y retning
+
+theta = 0; %startvinkel
+
+% LUFTMOTSTANDGREIER
+ro = 1.2923; %Lufttrykk ved 0*C
+C_d = 0.5; %Dragcoefficient for en kjegle
+A = pi * (d / 2) ^ 2; %Tversnittsareal for raketten oppover aka sirkel  
+
+k_l = (1/2) * ro * C_d * A; %luftmotstandkonstant
+
+
+
