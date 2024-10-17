e0e330cba09625d7c9fd195a99bfed132a852d59
Lars André Roda Jansen <larsandre03@gmail.com>
2024-10-17 16:00:11 +0200

    yuh

--- ttk4100-kybintro/ovinger/oving3/matlab/initverdier.m
+++ ttk4100-kybintro/ovinger/oving3/matlab/initverdier.m
@@ -9,7 +9,16 @@ theta_0 = 0;
 del = -5;
 u = 5;
 
-sim("oppg3d.slx")
 
-plot(out.x_t.Time, out.x_t.Data)
+%set_param([oppg3d '/Constant'], del, 10)
+set_param(['oppg3d' '/u'], 'Value', num2str(5))
+set_param(['oppg3d' '/del'], 'Value', num2str(-5))
+
+out = sim(model)
+
+hold on
+plot(out.x.Time, out.x.Data)
+%plot(out.z.Time, out.x.Data)
+hold off
+
 
