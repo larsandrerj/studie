4c101e4294e9ebb44dc1906756046f8f0427e762
Lars André Roda Jansen <larsandre03@gmail.com>
2024-10-18 09:07:52 +0200

    yuh

--- ttk4100-kybintro/ovinger/oving3/matlab/initverdier.m
+++ ttk4100-kybintro/ovinger/oving3/matlab/initverdier.m
@@ -10,15 +10,37 @@ del = -5;
 u = 5;
 
 
-%set_param([oppg3d '/Constant'], del, 10)
-set_param(['oppg3d' '/u'], 'Value', num2str(5))
-set_param(['oppg3d' '/del'], 'Value', num2str(-5))
+% Number of iterations for the loop
+num_iterations = 5; % Adjust as needed
 
-out = sim(model)
+% Prepare a figure for plotting
+figure;
+hold on;
 
-hold on
-plot(out.x.Time, out.x.Data)
-%plot(out.z.Time, out.x.Data)
-hold off
+for i = 1:num_iterations
+    % Set parameters
+    set_param(['oppg3d' '/u'], 'Value', num2str(u));
+    %set_param(['oppg3d' '/del'], 'Value', num2str(del));
+    
+    % Simulate the model
+    out = sim('oppg3d'); % Make sure the model name is correct
+
+    % Plot the results
+    %plot(out.x.Time, out.x.Data, 'DisplayName', ['u = ' num2str(u) ', del = ' num2str(del)]);
+    plot(out.x.Time, out.x.Data, 'DisplayName', ['u = ' num2str(u)]);
+    %plot(out.z.Time, out.z.Data, 'DisplayName', ['u = ' num2str(u) ', del = ' num2str(del)]);
+    
+    % Update the values for the next iteration
+    u = u + 2;
+    %del = del - 2;
+    %theta_0 = theta_0 +2 
+end
+
+hold off;
+xlabel('t');
+ylabel('x(t)');
+legend show; % Display legend for clarity
+title('theta_0 og del konst, u øker');
+
 
 
