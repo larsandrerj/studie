703820247f437ae7ff399908035ef04116bddd67
Lars André Roda Jansen <lajansen@stud.ntnu.no>
2025-02-06 11:59:08 +0100

    Thu Feb  6 11:59:08 CET 2025

--- ttk4101-imtek/motorlab/data/plotter.py
+++ ttk4101-imtek/motorlab/data/plotter.py
@@ -4,20 +4,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-file1 = "RTB2004_CHAN1.CSV"
-file2 = "RTB2004_CHAN2.CSV"
+file1 = "3_5d_pos.CSV"
+file2 = "3_5d_neg.CSV"
 
-file3 = "34c1.CSV"
 #leser csv-f
-data1 = pd.read_csv(file3)
-#data2 = pd.read_csv(file2)
+data1 = pd.read_csv(file1)
+data2 = pd.read_csv(file2)
 
 # Hent ut måleserie og tid, konverter til numpy
 time = data1.iloc[:,0].to_numpy()
 signal1 = data1.iloc[:,1].to_numpy()
-#signal2 = data2.iloc[:,1].to_numpy()
+signal2 = data2.iloc[:,1].to_numpy()
 
 plt.plot(time, signal1, 'red')
-#plt.plot(time, signal2, 'green')
+plt.plot(time, signal2, 'green')
 plt.grid()
 plt.show()
